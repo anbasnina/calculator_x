42a5244134b76047490bdd0eea5864c2189f127f
anastasiagame <anastasia.icloudgold@gmail.com>
2022-05-05 16:32:03 +0300

    spaces

--- Program.cs
+++ Program.cs
@@ -1,108 +1,100 @@
-﻿using System;
+﻿    using System;
 
-
-namespace calculator_x
-{
-    class MainClass
+    namespace calculator_x
     {
-        public static void Main(string[] args)
+        class MainClass
         {
-            double firstValue, secondValue;
-            double result = 0;
-            string action;
-            string resume = "N";
-
-
-
-            while (true)
+            public static void Main(string[] args)
             {
-                Console.WriteLine("Select an action: '+' '-' '*' '/' '%' '√' ");
-                action = Console.ReadLine();
+                double firstValue, secondValue;
+                double result = 0;
+                string action;
+                string resume = "N";
 
-                try
+                while (true)
                 {
-                    if (resume == "N")
+                    Console.WriteLine("Select an action: '+' '-' '*' '/' '%' '√' ");
+                    action = Console.ReadLine();
+
+                    try
                     {
-                    Console.WriteLine("Enter the first number");
-                    firstValue = double.Parse(Console.ReadLine());
-                    }
-                    else
-                    {
-                        firstValue = result;
-                    }
-                    
-                 
-                        Console.WriteLine("Enter the second number");
-                        secondValue = double.Parse(Console.ReadLine());
+                        if (resume == "N")
+                        {
+                        Console.WriteLine("Enter the first number");
+                        firstValue = double.Parse(Console.ReadLine());
+                        }
+                        else
+                        {
+                            firstValue = result;
+                        }
+                
+                            Console.WriteLine("Enter the second number");
+                            secondValue = double.Parse(Console.ReadLine());
                    
-                    
-
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine("Enter a number!");
-                    Console.ReadLine();
-                    continue;
-
-                }
-
-                if (action == "+")
-                {
-                    result = firstValue + secondValue;
-                    Console.WriteLine("Result:" + result);
-                }
-                else if (action == "-")
-                {
-                    result = firstValue - secondValue;
-                    Console.WriteLine("Result:" + result);
-                }
-                else if (action == "*")
-                {
-                    result = firstValue * secondValue;
-                    Console.WriteLine("Result:" + result);
-                }
-                else if (action == "/")
-                {
-                    if (secondValue == 0)
-                    {
-                        Console.WriteLine("Division by zero is impossible!");
                     }
-                    else
+                    catch (Exception)
                     {
-                        result = firstValue / secondValue;
+                        Console.WriteLine("Enter a number!");
+                        Console.ReadLine();
+                        continue;
+
+                    }
+
+                    if (action == "+")
+                    {
+                        result = firstValue + secondValue;
                         Console.WriteLine("Result:" + result);
                     }
-                }
-                else if (action == "%")
-                {
-                    result = firstValue * secondValue / 100;
-                    Console.WriteLine("Result:" + result + "%");
-                }
-                else if (action == "√")
-                {
-                    if( secondValue != 0)
+                    else if (action == "-")
                     {
-                    result = Math.Pow(firstValue, 1 / secondValue); ;
-                    Console.WriteLine("Result:" + result);
+                        result = firstValue - secondValue;
+                        Console.WriteLine("Result:" + result);
+                    }
+                    else if (action == "*")
+                    {
+                        result = firstValue * secondValue;
+                        Console.WriteLine("Result:" + result);
+                    }
+                    else if (action == "/")
+                    {
+                        if (secondValue == 0)
+                        {
+                            Console.WriteLine("Division by zero is impossible!");
+                        }
+                        else
+                        {
+                            result = firstValue / secondValue;
+                            Console.WriteLine("Result:" + result);
+                        }
+                    }
+                    else if (action == "%")
+                    {
+                        result = firstValue * secondValue / 100;
+                        Console.WriteLine("Result:" + result + "%");
+                    }
+                    else if (action == "√")
+                    {
+                        if( secondValue != 0)
+                        {
+                        result = Math.Pow(firstValue, 1 / secondValue); ;
+                        Console.WriteLine("Result:" + result);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Result:" + firstValue + "-> 0");
+                        }
                     }
                     else
                     {
-                        Console.WriteLine("Result:" + firstValue + "-> 0");
+                        Console.WriteLine("Error - unknown action!");
                     }
-                }
-                else
-                {
-                    Console.WriteLine("Error - unknown action!");
-                }
-            
 
-                Console.WriteLine("Resume 'Y' 'N' ");
-                resume = Console.ReadLine();
-
+                    Console.WriteLine("Resume 'Y' 'N' ");
+                    resume = Console.ReadLine();
          
+                }
+
             }
 
         }
-
     }
-}
